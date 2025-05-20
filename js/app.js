2a5ed995639f2736c79f245c207418825447b00d
frkngl <frknglww@gmail.com>
2025-05-20 12:21:33 +0300

    USER TARAFINDA MODAL SEKLINDE TAKIPCI VE TAKIP YERLERI YAPILDI.

--- js/app.js
+++ js/app.js
@@ -15,6 +15,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".tab-btnn");
+    const contents = document.querySelectorAll(".content-textt");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            // Aktif butonu güncelle
+            document.querySelector(".tab-btnn.active").classList.remove("active");
+            this.classList.add("active");
+
+            // İçerikleri güncelle
+            contents.forEach(content => content.classList.remove("active"));
+            document.getElementById(this.getAttribute("data-target")).classList.add("active");
+        });
+    });
+});
+
 document.querySelectorAll('nav a').forEach(link => {
     link.classList.remove('active');
 
